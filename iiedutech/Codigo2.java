f2391ea413dd2f59b38b29d188616184e64874c8
Renan RodriguesDev <renanrodrigues7110@gmail.com>
2025-08-19 20:43:41 -0300

    fix: sets in keywords

--- iiedutech/Codigo2.java
+++ iiedutech/Codigo2.java
@@ -1,26 +1,28 @@
 import java.util.Arrays;
+import java.util.TreeMap;
 
 public class Codigo2 {
 	public static void main(String[] args) {
 		// Produtos e seus respectivos preços: Xuxu:4.99 ... Maçã:3.99
-		String[] produtos = { "Xuxu", "Melão", "Goiaba", "Maçã" };
-		double[] precos = { 4.99, 2.99, 1.99, 3.99 };
+		TreeMap<String, Float> produtos_dict = new TreeMap<>();
+		produtos_dict.put("Xuxu", 4.99f);
+		produtos_dict.put("Melão", 2.99f);
+		produtos_dict.put("Goiaba", 1.99f);
+		produtos_dict.put("Maçã", 3.99f);
 
 		// BubbleSort
-		for (int x = 0; x < produtos.length - 1; x++) {
-			for (int y = 1; y < produtos.length; y++) {
-				if (produtos[x].compareTo(produtos[y]) > 0) {
-					String aux = produtos[x];
-					produtos[x] = produtos[y];
-					produtos[y] = aux;
-				}
-			}
-		}
+		// for (int x = 0; x < produtos.length - 1; x++) {
+		// for (int y = 1; y < produtos.length; y++) {
+		// if (produtos[x].compareTo(produtos[y]) > 0) {
+		// String aux = produtos[x];
+		// produtos[x] = produtos[y];
+		// produtos[y] = aux;
+		// }
+		// }
+		// }
 
-		Arrays.sort(produtos);
-		Arrays.sort(precos);
-		for (int cont = 0; cont < produtos.length; cont++) {
-			System.out.println(produtos[cont] + ": " + precos[cont]);
+		for (String produto : produtos_dict.keySet()) {
+			System.out.println(produto + ": " + produtos_dict.get(produto));
 		}
 	}
 }
